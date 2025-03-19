29637907083ef9bfa6edd5c268f55ee5196b6cae
Javier Smith <javier.smith.dlc@gmail.com>
2025-03-19 11:30:45 -0300

    chore: add langchain dependency

---
+++ src/intro.py
@@ -0,0 +1,11 @@
+import getpass
+import os
+
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+print(model.invoke("Hello world"))
